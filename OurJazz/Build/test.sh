b221356393a54262ef4e2de3ef1e8e8368bd5a2d
Arturgo <arturgo@hotmail.fr>
2020-10-22 23:32:03 +0200

    Compilation avec SFML

--- OurJazz/Build/test.sh
+++ OurJazz/Build/test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cp ../main.cpp main.cpp
 cp ../utils.h utils.h
-
+cp ../main.cpp main.cpp
 g++ -o main main.cpp -Wall -std=c++11 -O2
+
 ./main > proc.net
 ./../../Comp++/main proc.net > proc.h
-
 cp ../../Comp++/Runners/runner.cpp runner.cpp
-g++ -o proc runner.cpp -Wall -std=c++11 -Ofast -DOUTPUT -DINPUT
+
+g++ -o proc runner.cpp -Wall -std=c++11 -O2 -lsfml-window -lsfml-graphics -lsfml-system -DOUTPUT -DINPUT
