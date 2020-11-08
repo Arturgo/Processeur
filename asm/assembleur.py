f6e7474b328f001eb53565c4c5d84d770cb40eb8
etienne5 <etiennerossignol@outlook.com>
2020-11-08 22:06:27 +0100

    ajout de la possibilité d'importer un fichier

--- asm/assembleur.py
+++ asm/assembleur.py
@@ -1,8 +1,23 @@
 import sys
 
 r="from asm import *\n"
+sys.argv.append("Tests/test.s")
 with open(sys.argv[1]) as f:
-    t=list(f.readlines())
+    t=f.read()
+t+='\n'
+
+while t.find("import ")!=-1:
+    pos=t.find("import ")
+    nom=t[pos+len("import "):]
+    nom=nom[:nom.find('\n')]
+    with open(nom) as f:
+        v=f.read()
+    t=t.replace("import "+nom, v)
+
+t=list(map(lambda x : x+'\n',t.split('\n')))
+
+
+
 for el in t:
     r+="""try:
     None
@@ -14,4 +29,5 @@ r+="\nInit()\n"
 r+=''.join(t)
 r+="\nEcrire()\n"
 r+=''.join(t)
+
 exec(r)
